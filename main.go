c2db0e5f3b717194cac750ec24be29760b9b26b9
Kalissaac <kalissaac@protonmail.com>
2022-02-16 12:33:15 -0800

    Add main

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+type Color int
+
+const (
+	Unknown Color = iota
+	Red
+	Orange
+	Yellow
+	Green
+	Blue
+	Purple
+)
+
+func (c Color) String() string {
+	colors := [...]string{"red", "orange", "yellow", "green", "blue", "purple"}
+	if c < Red || c > Purple {
+		return fmt.Sprintf("Color(%d)", int(c))
+	}
+	return colors[c-Red]
+}
+
+// Returns a color from Color when passed a hex color code, defaults to Unknown
+func GetColorFromHex(hexColor string) Color {
+	return Color(0)
+}
