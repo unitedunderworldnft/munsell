e341189e87bc4b752297d415e4e858daa6bed9f0
Kalissaac <kalissaac@protonmail.com>
2022-02-18 18:05:53 -0800

    Add stub functions

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ const (
 	Purple
 )
 
+type RGBComponents struct {
+	Red, Green, Blue uint8
+}
+
 func (c Color) String() string {
 	colors := [...]string{"red", "orange", "yellow", "green", "blue", "purple"}
 	if c < Red || c > Purple {
@@ -24,5 +28,17 @@ func (c Color) String() string {
 
 // Returns a color from Color when passed a hex color code, defaults to Unknown
 func GetColorFromHex(hexColor string) Color {
+	return Color(0)
+}
+
+func EnsureHexIsValid(hexColor string) bool {
+	return true
+}
+
+func GetRGBFromHex(hexColor string) RGBComponents {
+	return RGBComponents{}
+}
+
+func MatchColorToRGB(rgb RGBComponents) Color {
 	return Color(0)
 }
